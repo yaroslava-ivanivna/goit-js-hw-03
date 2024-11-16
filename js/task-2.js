135f8fb13ddd234e0de2a77e82f80bbabd044c44
Yaroslava <yaroslava.ivanivna9@gmail>
2024-11-16 14:56:31 +0200

    hometask

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const arrayResult = firstArray.concat(secondArray);
+  if (arrayResult.length >= maxLength) {
+    return arrayResult.slice(0, maxLength);
+  } else {
+    return arrayResult;
+  }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
